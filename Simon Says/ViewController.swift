290e11d8c53a23c8172d029f25de4220af7fcdce
Derrickcod <dcodlyn@protonmail.ch>
2018-04-05 13:29:43 -0500

    Added tags to buttons and implemented user feedback on color buttons.

--- Simon Says/ViewController.swift
+++ Simon Says/ViewController.swift
@@ -18,13 +18,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //sorts the order of the tags
+        colorButtons = colorButtons.sorted() {
+            $0.tag < $1.tag
+        }
+        
+        playerLabels = playerLabels.sorted() {
+            $0.tag < $1.tag
+        }
+        
+        scoreLabels = scoreLabels.sorted() {
+            $0.tag < $1.tag
+        }
+        
+        createNewGame()
+        
+    }
+    
+    func createNewGame() {
+        actionButton.setTitle("Start Game", for: .normal)
+        
+        for button in colorButtons {
+            button.alpha = 0.5
+        }
     }
 
     @IBAction func colorButtonHandler(_ sender: CircularButton) {
+        print("button \(sender.tag) tapped")
     }
     
     @IBAction func actionButtonHandler(_ sender: UIButton) {
+        print("Action Button pressed")
     }
     
 }
